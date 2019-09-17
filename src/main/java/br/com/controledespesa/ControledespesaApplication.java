d0751fc19c1ba29dab1ee9457f983551ee4466a1
ecardoso <everton.ap.cardoso@gmail.com>
2019-09-17 15:53:11 -0300

    EnableAutoConfiguration

--- src/main/java/br/com/controledespesa/ControledespesaApplication.java
+++ src/main/java/br/com/controledespesa/ControledespesaApplication.java
@@ -1,9 +1,11 @@
 package br.com.controledespesa;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class ControledespesaApplication {
 
 	public static void main(String[] args) {
