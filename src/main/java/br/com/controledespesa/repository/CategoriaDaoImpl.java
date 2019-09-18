c4ede1fe195b590b3e0b694732a442ce780304c9
ecardoso <everton.ap.cardoso@gmail.com>
2019-09-18 08:38:24 -0300

    voltando bd h2

--- src/main/java/br/com/controledespesa/repository/CategoriaDaoImpl.java
+++ src/main/java/br/com/controledespesa/repository/CategoriaDaoImpl.java
@@ -1,8 +1,10 @@
 package br.com.controledespesa.repository;
 
+import org.springframework.stereotype.Repository;
+
 import br.com.controledespesa.entity.Categoria;
 
-//@Repository
+@Repository
 public class CategoriaDaoImpl extends GenericDao<Categoria, Long> {
 
 }
--- src/main/java/br/com/controledespesa/rest/CategoriaService.java
+++ src/main/java/br/com/controledespesa/rest/CategoriaService.java
@@ -3,16 +3,22 @@ package br.com.controledespesa.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.controledespesa.entity.Categoria;
+import br.com.controledespesa.repository.CategoriaDaoImpl;
 
 @RestController
 public class CategoriaService {
 
-	//@Autowired
-	//private CategoriaDaoImpl categoriaDao;
+	@Autowired
+	private CategoriaDaoImpl categoriaDao;
 
 	@RequestMapping(value = "/categoriaLista")
 	public List<Categoria> lista() {
@@ -25,16 +31,16 @@ public class CategoriaService {
 		//return (List<Categoria>) categoriaDao.findAll();
 	}
 
-	/*@RequestMapping(value = "/getCategoria")
+	@RequestMapping(value = "/getCategoria")
 	public Categoria getCategoria(@RequestParam(value = "id", defaultValue = "1") Long id) {
 		Categoria categoria = categoriaDao.getById(id);
 		return categoria;
 	}
-	
+
 	@RequestMapping(value = "/categoriaSalvar", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public Categoria categoriaSalvar(@RequestBody Categoria categoria) {
 		categoriaDao.save(categoria);
 		return categoria;
-	}*/
+	}
 
 }
