8ff0d1f0e6209d705173d3b151cbe4af773638d5
ecardoso <everton.ap.cardoso@gmail.com>
2020-05-06 20:55:51 -0300

    Update DespesaController.java
    trocar parametro do método

--- src/main/java/br/com/controledespesa/rest/DespesaController.java
+++ src/main/java/br/com/controledespesa/rest/DespesaController.java
@@ -125,18 +125,17 @@ public class DespesaController implements Serializable {
 	@PostMapping(value = "/updateDespesaParaPagoByMes", consumes = MediaType.APPLICATION_JSON_VALUE)
 	@ApiResponses(value = { @ApiResponse(code = 200, message = "atualizar as despasa para pago do mês referente"),
 			@ApiResponse(code = 403, message = "Você não tem permissão para acessar este recurso"), @ApiResponse(code = 500, message = "Foi gerada uma exceção"), })
-	@ApiImplicitParams({ @ApiImplicitParam(name = "usuario", value = "id do usuário", required = true, dataTypeClass = String.class),
-			@ApiImplicitParam(name = "data", value = "data", required = true, dataTypeClass = String.class) })
-	public void updateDespesaParaPagoByMes(@RequestParam(value = "usuario") String idUsuario, @RequestParam(value = "data") String dataParam) throws ParseException {
-		Date data = DataHelper.converterStringParaDate(dataParam);
+	@ApiImplicitParams({ @ApiImplicitParam(name = "despesa", value = "despesa", required = true, dataTypeClass = Despesa.class) })
+	public void updateDespesaParaPagoByMes(@RequestBody Despesa despesa) {
+		Date data = despesa.getDataCompra();
 		Date dataInicial = DataHelper.getPrimeiroDiaDoMes(data);
 		Date dataFinal = DataHelper.getUltimoDiaDoMes(data);
 
-		List<Despesa> despesas = despesaDao.findByMes(idUsuario, dataInicial, dataFinal);
-		for (Despesa despesa : despesas) {
-			if (!despesa.isPago()) {
-				despesa.setPago(true);
-				despesaDao.update(despesa);
+		List<Despesa> despesas = despesaDao.findByMes(despesa.getUsuario().getId().toString(), dataInicial, dataFinal);
+		for (Despesa value : despesas) {
+			if (!value.isPago()) {
+				value.setPago(true);
+				despesaDao.update(value);
 			}
 		}
 	}
